1ea572605f537c026c213c5651eb1d44d95714f3
[Josh Shepherd] <[joshuashepherd877@gmail.com]>
2026-01-28 13:28:49 +0000

    create jwt token route for logging in

--- core/__init__.py
+++ core/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from .extensions import db, ma, migrate
+from .extensions import db, ma, migrate, jwt
 from .models import Post, User
 from .routes.blog import blog_bp
 from .routes.users import users
+from .routes.jwt import jwt_bp
 
 
 def create_app():
@@ -10,14 +11,17 @@ def create_app():
     # config
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = "DJFOIEFREIF"
 
     # initialize extensions
     db.init_app(app)
     ma.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
 
     # register blueprints
     app.register_blueprint(blog_bp)
     app.register_blueprint(users)
+    app.register_blueprint(jwt_bp)
 
     return app
---
+++ core/routes/jwt.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import check_password_hash
+from ..models import User
+
+jwt_bp = Blueprint("jwt_bp", __name__, url_prefix="/jwt")
+
+
+@jwt_bp.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user or not check_password_hash(user.password_hash, password):
+        return jsonify({"error": "Incorrect username or password"})
+
+    access_token = create_access_token(identity=username)
+
+    return jsonify(access_token)
+
+
+@jwt_bp.route("/", methods=["GET"])
+@jwt_required()
+def test_logged_in():
+    current_user = get_jwt_identity()
+
+    return jsonify({"Message": "You are logged in", "User": current_user})
